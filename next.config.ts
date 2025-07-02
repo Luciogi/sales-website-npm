c1c814aa07ef9b2fd48ad8490515d2a44c94ba04
Luciogi <84625092+Luciogi@users.noreply.github.com>
2025-07-02 13:51:56 +0500

    disable lint on build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
